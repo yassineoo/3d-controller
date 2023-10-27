efb7c9b27ceafd568f6739ae2d2c14f17b1f8cba
yassineoo <jy_attou@esi.dz>
2023-10-27 10:42:00 +0100

    fix qr code types2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 import Home3D from "@/components/try";
 import Head from "next/head";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
+import { useRouter } from "next/router";
 
 export default function Home() {
   const router = useRouter();
