b67c8d0c003663d9752c52b9b4b8f815ff9c2fb0
yassineoo <jy_attou@esi.dz>
2025-06-13 20:13:49 +0100

    push

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,46 +1,5 @@
-"use client";
-import Head from "next/head";
-import { useRouter } from "next/router";
-
-type Content = {
-  title: string;
-  query: string;
+const xss = () => {
+  return <div>ss</div>;
 };
 
-function ConnectButton({ title, query }: Content) {
-  const router = useRouter();
-  return (
-    <button
-      type="button"
-      className="w-25 h-10 bg-blue-500 hover:bg-blue-700 text-white font-bold rounded"
-      onClick={() => {
-        router.push({
-          pathname: "/connect",
-          query: { component: query },
-        });
-      }}
-    >
-      {title}
-    </button>
-  );
-}
-
-export default function Home() {
-  return (
-    <div className="bg-white">
-      <Head>
-        <title>Next.js + Three.js</title>
-      </Head>
-      <header className="bg-blue-600">
-        <h1 className="font-extrabold   text-xl">My Next.js + Three.js App</h1>
-      </header>
-      <main className="bg-red flex flex-col">
-        <div className="flex flex-row justify-around h-96 items-center">
-          <ConnectButton title="Desktop" query="desktop_pc" />
-          <ConnectButton title="planet" query="planet" />
-          <ConnectButton title="swimvilla" query="swimvilla" />
-        </div>
-      </main>
-    </div>
-  );
-}
+export default xss;
